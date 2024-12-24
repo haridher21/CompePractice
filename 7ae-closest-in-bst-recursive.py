0ffe051ad74fac2741749c223330a72517a0bea3
Haridher Pandiyan <48638897+haridher21@users.noreply.github.com>
2024-12-24 15:48:03 -0600

    Update and rename 7ae.py to 7ae-closest-in-bst-recursive.py

---
+++ 7ae-closest-in-bst-recursive.py
@@ -0,0 +1,23 @@
+def findClosestValueInBst(tree, target): # Done recursively, do iterative
+    return findClosestValue(tree, target, tree.value)
+
+def findClosestValue(tree, target, min):
+    if tree.value == target:
+        return target
+    if target < tree.value:
+        if tree.left:
+            if abs(tree.left.value - target) < abs(min - target):
+                min = tree.left.value
+            return findClosestValue(tree.left, target, min)
+    else:
+        if tree.right:
+            if abs(tree.right.value - target) < abs(min - target):
+                min = tree.right.value
+            return findClosestValue(tree.right, target, min)
+    return min
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
