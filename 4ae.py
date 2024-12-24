ee401909b896da06a650246a63e87ee0c2ee04cd
Haridher Pandiyan <48638897+haridher21@users.noreply.github.com>
2024-12-23 20:42:52 -0600

    Update 4ae.py

--- 4ae.py
+++ 4ae.py
@@ -1,1 +1,18 @@
-## Will add the code once I login again
+def tournamentWinner(competitions, results):
+    d, n = {}, len(results)
+    for i in range(n):
+        if competitions[i][0] not in d:
+            d[competitions[i][0]] = 0
+        if competitions[i][1] not in d:
+            d[competitions[i][1]] = 0
+        if results[i] == 1:
+            d[competitions[i][0]] += 3
+        else:
+            d[competitions[i][1]] += 3
+    maxval = -1
+    for k in d:
+        if d[k] > maxval:
+            maxval = d[k]
+            winner = k
+    return winner
+            
