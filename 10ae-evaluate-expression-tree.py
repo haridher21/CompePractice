ca374f68a18d2042e20560058cbb169b75522ad0
Haridher Pandiyan <48638897+haridher21@users.noreply.github.com>
2024-12-28 11:13:30 -0600

    Update and rename 10ae.py to 10ae-evaluate-expression-tree.py

---
+++ 10ae-evaluate-expression-tree.py
@@ -0,0 +1,14 @@
+def evaluateExpressionTree(tree):
+    return et(tree)
+
+def et(tree):
+    if not tree.value < 0:
+        return tree.value
+    if tree.value == -1:
+        return et(tree.left) + et(tree.right)
+    elif tree.value == -2:
+        return et(tree.left) - et(tree.right)
+    elif tree.value == -3:
+        return int(et(tree.left) / et(tree.right))
+    elif tree.value == -4:
+        return et(tree.left) * et(tree.right)
