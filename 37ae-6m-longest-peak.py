2937af7c975b7f6fda5e4cdda8bb9d1cc8294a4c
Haridher Pandiyan <48638897+haridher21@users.noreply.github.com>
2024-12-30 01:04:56 -0600

    Create 37ae-6m-longest-peak.py

---
+++ 37ae-6m-longest-peak.py
@@ -0,0 +1,19 @@
+def longestPeak(array): # O(N)
+    r, d, peak = 0, 0, 0
+    for i in range(len(array) - 1):
+        if array[i] < array[i + 1]:
+            if d > 0:
+                peak = max(peak, r + d + 1)
+                r, d = 0, 0
+            r += 1
+        elif array[i] > array[i + 1]:
+            if r > 0:
+                d += 1
+        elif array[i] == array[i + 1]:
+            if d > 0:
+                peak = max(peak, r + d + 1)
+            r, d = 0, 0
+    if d > 0:
+        peak = max(peak, r + d + 1)
+    return peak
+                
