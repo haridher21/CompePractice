433fb2a20e0fbc6370158b9b0a6f523fd288f858
Haridher Pandiyan <48638897+haridher21@users.noreply.github.com>
2024-12-30 12:14:05 -0600

    Create 40ae-9m-merge-intervals.py

---
+++ 40ae-9m-merge-intervals.py
@@ -0,0 +1,20 @@
+def mergeOverlappingIntervals(intervals): # O(NlogN) T O(N) S
+    if len(intervals):
+        intervals.sort(key = lambda interval: interval[0])
+        final = [intervals[0]]
+    else:
+        return []
+
+    for i in range(1, len(intervals)):
+        j = len(final) - 1
+        if intervals[i][1] < final[j][0]:
+            final.append(intervals[i])
+            continue
+        if intervals[i][0] > final[j][1]:
+            final.append(intervals[i])
+            continue
+        if intervals[i][0] < final[j][0]:
+            final[j][0] = intervals[i][0]
+        if intervals[i][1] > final[j][1]:
+            final[j][1] = intervals[i][1]
+    return final
