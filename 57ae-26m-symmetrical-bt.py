08a21ab65316ea432e9a9d885b9ed99ea06986c3
Haridher Pandiyan <48638897+haridher21@users.noreply.github.com>
2025-01-03 17:01:39 -0600

    Create 57ae-26m-symmetrical-bt.py

---
+++ 57ae-26m-symmetrical-bt.py
@@ -0,0 +1,22 @@
+# Can be written significantly shorter lol
+def symmetricalTree(tree): # O(N) T O(H) S (space is just stack height) Optimal
+    return st(tree, tree)
+
+def st(tree1, tree2):
+    c1, c2 = True, True
+    if tree1.left:
+        if not(tree2.right and tree1.left.value == tree2.right.value):
+            return False
+        c1 = st(tree1.left, tree2.right)
+    elif tree2.right:
+        return False
+    if tree2.left:
+        if not(tree1.right and tree2.left.value == tree1.right.value):
+            return False
+        c2 = st(tree1.right, tree2.left)
+    elif tree1.right:
+        return False
+    
+    if c1 and c2:
+        return True
+    return False
