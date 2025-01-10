ecb552fd7c1188145cb5dc0f015f33b40e5bb08e
Haridher Pandiyan <48638897+haridher21@users.noreply.github.com>
2025-01-09 20:21:00 -0600

    Create 64ae-33m-max-subarray-sum.py

---
+++ 64ae-33m-max-subarray-sum.py
@@ -0,0 +1,12 @@
+def kadanesAlgorithm(array): # O(N) T O(1) S
+    n = len(array)
+    if n == 0:
+        return None
+    maxv, runningSum = array[0], array[0]
+    for i in range(1, n):
+        if runningSum + array[i] < array[i]:
+            runningSum = array[i]
+        else:
+            runningSum += array[i]
+        maxv = max(maxv, runningSum)
+    return maxv
