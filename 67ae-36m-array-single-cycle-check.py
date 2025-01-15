d1616b0deaea432579801dd6d7957958f4853051
Haridher Pandiyan <48638897+haridher21@users.noreply.github.com>
2025-01-15 07:25:26 -0600

    Create 67ae-36m-array-single-cycle-check.py

---
+++ 67ae-36m-array-single-cycle-check.py
@@ -0,0 +1,20 @@
+def hasSingleCycle(array): # O(N) T O(1) S
+    n , count = len(array), 1
+    loc = (array[0]) % n
+    while count < n and loc != 0:
+        count += 1
+        loc = (loc + array[loc]) % n
+    return True if loc == 0 and count == n else False
+
+"""
+def hasSingleCycle(array): # O(N) TS
+    n, count, loc = len(array), 0, 0
+    visited = [False] * n
+    while count < n:
+        if visited[loc] == True:
+            return False
+        visited[loc] = True
+        count += 1
+        loc = (loc + array[loc]) % n
+    return True if loc == 0 else False
+"""
