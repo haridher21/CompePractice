7edf4abc02d3a3836bcc0c1a1a30123ca08b1418
Haridher Pandiyan <48638897+haridher21@users.noreply.github.com>
2025-01-19 21:30:22 -0600

    Create 72ae-41m-graph-cycle.py

---
+++ 72ae-41m-graph-cycle.py
@@ -0,0 +1,27 @@
+def cycleInGraph(edges):
+    # Optimal, I'm sure, atleast timewise. Don't think I can do it iteratively
+    n = len(edges)
+    riplist = []
+    for i in range(n):
+        visited = [False for _ in range(n)]
+        riplist = dfs(i, visited, [], edges, riplist)
+        if type(riplist) == bool:
+            return True
+    return False
+        
+def dfs(cur, visited, path, edges, riplist):
+    if visited[cur]:
+        return True
+    if cur in riplist:
+        return riplist
+    path.append(cur)
+    visited[cur] = True
+    for edge in edges[cur]:
+        riplist = dfs(edge, visited, path, edges, riplist)
+        if type(riplist) == bool:
+            return True
+    path.pop()
+    visited[cur] = False
+    riplist.append(cur)
+    return riplist
+    
