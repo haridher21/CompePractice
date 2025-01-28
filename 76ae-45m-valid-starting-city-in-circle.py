1c1adb5c829575a05927475eab8fb70909a40947
Haridher Pandiyan <48638897+haridher21@users.noreply.github.com>
2025-01-27 20:38:03 -0600

    Update 76ae-45m-valid-starting-city-in-circle.py

--- 76ae-45m-valid-starting-city-in-circle.py
+++ 76ae-45m-valid-starting-city-in-circle.py
@@ -1,4 +1,8 @@
 def validStartingCity(distances, fuel, mpg): # O(N) T O(1) S
+    # Different from all proposed solutions, makes me wonder if mine could fail
+    # My reasoning essentially was, from a given starting point, if I essentially hit any negatives,
+    # means we cannot start from wherever we started, and must look ahead. And resetting to 0, means we
+    # starting from there, so it should be fine
     running_sum, start_index = 0, 0
     for i in range(len(fuel)):
         running_sum += (fuel[i] * mpg - distances[i])
